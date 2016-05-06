0f49b424415e0b17957976e698c899d06faa3368
Hanno Hecker <hanno.hecker@zalando.de>
2016-05-06 08:38:50 +0200

    add proxy auth whoami example

--- whoami_test.go
+++ whoami_test.go
@@ -2,6 +2,7 @@ package ldap_test
 
 import (
 	"fmt"
+
 	"gopkg.in/ldap.v2"
 )
 
@@ -23,5 +24,28 @@ func ExampleWhoAmI() {
 	if err != nil {
 		fmt.Errorf("Failed to call WhoAmI(): %s\n", err)
 	}
-	fmt.Printf("%s\n", res.AuthzId)
+	fmt.Printf("I am: %s\n", res.AuthzId)
+}
+
+func ExampleWhoAmIProxied() {
+	conn, err := ldap.Dial("tcp", "ldap.example.org:389")
+	if err != nil {
+		fmt.Errorf("Failed to connect: %s\n", err)
+	}
+
+	_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
+		Username: "uid=someone,ou=people,dc=example,dc=org",
+		Password: "MySecretPass",
+	})
+	if err != nil {
+		fmt.Errorf("Failed to bind: %s\n", err)
+	}
+
+	pa := ldap.NewProxyAuthControl("dn:uid=other,ou=people,dc=example,dc=org")
+
+	res, err := conn.WhoAmI([]ldap.Control{pa})
+	if err != nil {
+		fmt.Errorf("Failed to call WhoAmI(): %s\n", err)
+	}
+	fmt.Printf("For this call only I am now: %s\n", res.AuthzId)
 }
