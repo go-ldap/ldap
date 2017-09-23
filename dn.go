db4cd3d96bca815cd7bba331bff55d459f0c3e67
Jud White <github@judsonwhite.com>
2017-09-23 03:15:27 -0500

    fix typos

--- dn.go
+++ dn.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 //
-// File contains DN parsing functionallity
+// File contains DN parsing functionality
 //
 // https://tools.ietf.org/html/rfc4514
 //
