c6ce48312dc899d500d77926d9d3c33a0e01259c
Christian Roggia <christian.roggia@gmail.com>
2018-07-03 17:26:01 +0200

    Fix unittests pointing to the external package "gopkg.in/ldap.v2" (#176)
    * Fix unittests pointing to "gopkg.in/ldap.v2"
    
    * Restore URLs containing "ldap.*"
    
    * Remove "gopkg.in/ldap.v2" from Travis CI

--- moddn_test.go
+++ moddn_test.go
@@ -1,20 +1,18 @@
-package ldap_test
+package ldap
 
 import (
 	"log"
-
-	"gopkg.in/ldap.v2"
 )
 
 // ExampleConn_ModifyDN_renameNoMove shows how to rename an entry without moving it
 func ExampleConn_ModifyDN_renameNoMove() {
-	conn, err := ldap.Dial("tcp", "ldap.example.org:389")
+	conn, err := Dial("tcp", "ldap.example.org:389")
 	if err != nil {
 		log.Fatalf("Failed to connect: %s\n", err)
 	}
 	defer conn.Close()
 
-	_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
+	_, err = conn.SimpleBind(&SimpleBindRequest{
 		Username: "uid=someone,ou=people,dc=example,dc=org",
 		Password: "MySecretPass",
 	})
@@ -22,7 +20,7 @@ func ExampleConn_ModifyDN_renameNoMove() {
 		log.Fatalf("Failed to bind: %s\n", err)
 	}
 	// just rename to uid=new,ou=people,dc=example,dc=org:
-	req := ldap.NewModifyDNRequest("uid=user,ou=people,dc=example,dc=org", "uid=new", true, "")
+	req := NewModifyDNRequest("uid=user,ou=people,dc=example,dc=org", "uid=new", true, "")
 	if err = conn.ModifyDN(req); err != nil {
 		log.Fatalf("Failed to call ModifyDN(): %s\n", err)
 	}
@@ -30,13 +28,13 @@ func ExampleConn_ModifyDN_renameNoMove() {
 
 // ExampleConn_ModifyDN_renameAndMove shows how to rename an entry and moving it to a new base
 func ExampleConn_ModifyDN_renameAndMove() {
-	conn, err := ldap.Dial("tcp", "ldap.example.org:389")
+	conn, err := Dial("tcp", "ldap.example.org:389")
 	if err != nil {
 		log.Fatalf("Failed to connect: %s\n", err)
 	}
 	defer conn.Close()
 
-	_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
+	_, err = conn.SimpleBind(&SimpleBindRequest{
 		Username: "uid=someone,ou=people,dc=example,dc=org",
 		Password: "MySecretPass",
 	})
@@ -45,7 +43,7 @@ func ExampleConn_ModifyDN_renameAndMove() {
 	}
 	// rename to uid=new,ou=people,dc=example,dc=org and move to ou=users,dc=example,dc=org ->
 	// uid=new,ou=users,dc=example,dc=org
-	req := ldap.NewModifyDNRequest("uid=user,ou=people,dc=example,dc=org", "uid=new", true, "ou=users,dc=example,dc=org")
+	req := NewModifyDNRequest("uid=user,ou=people,dc=example,dc=org", "uid=new", true, "ou=users,dc=example,dc=org")
 
 	if err = conn.ModifyDN(req); err != nil {
 		log.Fatalf("Failed to call ModifyDN(): %s\n", err)
@@ -54,13 +52,13 @@ func ExampleConn_ModifyDN_renameAndMove() {
 
 // ExampleConn_ModifyDN_moveOnly shows how to move an entry to a new base without renaming the RDN
 func ExampleConn_ModifyDN_moveOnly() {
-	conn, err := ldap.Dial("tcp", "ldap.example.org:389")
+	conn, err := Dial("tcp", "ldap.example.org:389")
 	if err != nil {
 		log.Fatalf("Failed to connect: %s\n", err)
 	}
 	defer conn.Close()
 
-	_, err = conn.SimpleBind(&ldap.SimpleBindRequest{
+	_, err = conn.SimpleBind(&SimpleBindRequest{
 		Username: "uid=someone,ou=people,dc=example,dc=org",
 		Password: "MySecretPass",
 	})
@@ -68,7 +66,7 @@ func ExampleConn_ModifyDN_moveOnly() {
 		log.Fatalf("Failed to bind: %s\n", err)
 	}
 	// move to ou=users,dc=example,dc=org -> uid=user,ou=users,dc=example,dc=org
-	req := ldap.NewModifyDNRequest("uid=user,ou=people,dc=example,dc=org", "uid=user", true, "ou=users,dc=example,dc=org")
+	req := NewModifyDNRequest("uid=user,ou=people,dc=example,dc=org", "uid=user", true, "ou=users,dc=example,dc=org")
 	if err = conn.ModifyDN(req); err != nil {
 		log.Fatalf("Failed to call ModifyDN(): %s\n", err)
 	}
