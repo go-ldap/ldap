3633b98fbb6780d30ae11e3dbf6d1f22fa65e72f
Clément Blaise <blaise_c@prodgrp.local>
2021-01-18 18:14:46 +0100

    Add Entry Unmarshal

--- search.go
+++ search.go
@@ -3,6 +3,7 @@ package ldap
 import (
 	"errors"
 	"fmt"
+	"reflect"
 	"sort"
 	"strings"
 
@@ -159,6 +160,87 @@ func (e *Entry) PrettyPrint(indent int) {
 	for _, attr := range e.Attributes {
 		attr.PrettyPrint(indent + 2)
 	}
+}
+
+// Describe the tag to use for struct annotation
+const decoderTagName = "ldap"
+
+// Read Struct Field to return the associated Tag name
+// omitempty can be specified
+func readTag(f reflect.StructField) (string, bool) {
+	val, ok := f.Tag.Lookup(decoderTagName)
+	if !ok {
+		return f.Name, false
+	}
+	opts := strings.Split(val, ",")
+	omit := false
+	if len(opts) == 2 {
+		omit = opts[1] == "omitempty"
+	}
+	return opts[0], omit
+}
+
+// Unmarshal take an struct to be filled with entry result
+func (e *Entry) Unmarshal(i interface{}) (err error) {
+	// Make sure it's a ptr
+	if reflect.ValueOf(i).Kind() != reflect.Ptr {
+		return fmt.Errorf("expecting a ptr not %s", reflect.ValueOf(i).Kind())
+
+	}
+	// Get ptr value and type
+	v, t := reflect.ValueOf(i).Elem(), reflect.TypeOf(i).Elem()
+
+	// Make sure it's pointing to a struct
+	if v.Kind() != reflect.Struct {
+		return fmt.Errorf("expecting a ptr to a struct not %s", v.Kind())
+	}
+
+	// Go trough all the struct fields
+	for n := 0; n < t.NumField(); n++ {
+		// Holds field value and type
+		fv, ft := v.Field(n), t.Field(n)
+		// skip unexported fields
+		if ft.PkgPath != "" {
+			continue
+		}
+
+		// Read tag from field type
+		tag, _ := readTag(ft)
+
+		// All entry have dn
+		if tag == "dn" {
+			fv.SetString(e.DN)
+			continue
+		}
+
+		// Go trough other attributes
+		for _, attr := range e.Attributes {
+
+			// Match attributes
+			if attr.Name == tag {
+
+				// Result is empty
+				if len(attr.Values) == 0 {
+					continue
+				}
+
+				// Result is a string
+				if len(attr.Values) == 1 && fv.Kind() == reflect.String {
+					fv.SetString(attr.Values[0])
+					continue
+				}
+
+				// Result is a slice
+				if len(attr.Values) > 1 && fv.Kind() == reflect.Slice {
+					for _, item := range attr.Values {
+						fv.Set(reflect.Append(fv, reflect.ValueOf(item)))
+					}
+				}
+
+			}
+		}
+	}
+	return
 }
 
 // NewEntryAttribute returns a new EntryAttribute with the desired key-value pair
