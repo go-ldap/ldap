2fcbf00244024dec3e254dfddd97e5eb807b3374
n3integration <n3integration@gmail.com>
2022-04-05 20:00:26 -0400

    [#365] - support for configurable logger

--- v3/del.go
+++ v3/del.go
@@ -1,8 +1,6 @@
 package ldap
 
 import (
-	"log"
-
 	ber "github.com/go-asn1-ber/asn1-ber"
 )
 
@@ -53,7 +51,7 @@ func (l *Conn) Del(delRequest *DelRequest) error {
 			return err
 		}
 	} else {
-		log.Printf("Unexpected Response: %d", packet.Children[1].Tag)
+		logger.Printf("Unexpected Response: %d", packet.Children[1].Tag)
 	}
 	return nil
 }
