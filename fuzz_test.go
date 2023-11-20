b47d9e3353cee1c24f343035f6873d0fc0613a6f
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2023-11-20 10:04:30 +0900

    fix: add a setup process in TestMain to prevent DATA RACE as a result of changing ber's module global variable for fuzz tests #472

--- fuzz_test.go
+++ fuzz_test.go
@@ -4,15 +4,23 @@
 package ldap
 
 import (
-	ber "github.com/go-asn1-ber/asn1-ber"
+	"os"
 	"testing"
+
+	ber "github.com/go-asn1-ber/asn1-ber"
 )
 
-func FuzzParseDN(f *testing.F) {
+func TestMain(m *testing.M) {
+	// For fuzz tests
 	// See https://github.com/go-asn1-ber/asn1-ber/blob/04301b4b1c5ff66221f8f8a394f814a9917d678a/fuzz_test.go#L33-L37
 	// for why this limitation is necessary
 	ber.MaxPacketLengthBytes = 65536
 
+	code := m.Run()
+	os.Exit(code)
+}
+
+func FuzzParseDN(f *testing.F) {
 	f.Add("*")
 	f.Add("cn=Jim\\0Test")
 	f.Add("cn=Jim\\0")
