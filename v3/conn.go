f09ee91fc0c33a20ff2812663ce058cc329ba396
Christopher Puschmann <cp@lumen.sh>
2024-04-01 20:53:07 +0200

    chore: add comment to `Start` function to inform about possible instability when called (#499)

--- v3/conn.go
+++ v3/conn.go
@@ -265,7 +265,12 @@ func NewConn(conn net.Conn, isTLS bool) *Conn {
 	return l
 }
 
-// Start initializes goroutines to read responses and process messages
+// Start initialises goroutines to read replies and process messages.
+//
+// Deprecated: It is usually not necessary to call this function
+// manually. It is public for compatibility reasons and may
+// cause a race condition when processing messages.
+// See: https://github.com/go-ldap/ldap/issues/356
 func (l *Conn) Start() {
 	go l.reader()
 	go l.processMessages()
