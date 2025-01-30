a55d800e4c5105fa024a1f3b6b1004cc780b7543
kozlov-e <kozlov-e@protei-lab.ru>
2025-01-30 06:31:34 +0300

    fix: handle missing sorting control in OpenLDAP response

--- v3/control.go
+++ v3/control.go
@@ -755,7 +755,7 @@ func NewControlServerSideSorting(value *ber.Packet) (*ControlServerSideSorting, 
 	sequences := val[0].Children
 
 	for i, sequence := range sequences {
-		sortKey := &SortKey{}
+		sortKey := new(SortKey)
 
 		if len(sequence.Children) < 2 {
 			return nil, fmt.Errorf("attributeType or matchingRule is missing from sequence %d", i)
@@ -864,10 +864,11 @@ func (c ControlServerSideSortingCode) Valid() error {
 }
 
 func NewControlServerSideSortingResult(pkt *ber.Packet) (*ControlServerSideSortingResult, error) {
-	control := &ControlServerSideSortingResult{}
+	control := new(ControlServerSideSortingResult)
 
 	if pkt == nil || len(pkt.Children) == 0 {
-		return nil, fmt.Errorf("bad packet")
+		// Фикс бага, наличие Children для OpenLdap не обязательно.
+		return control, nil
 	}
 
 	codeInt, err := ber.ParseInt64(pkt.Children[0].Data.Bytes())
@@ -875,8 +876,7 @@ func NewControlServerSideSortingResult(pkt *ber.Packet) (*ControlServerSideSorti
 		return nil, err
 	}
 
-	code := ControlServerSideSortingCode(codeInt)
-	if err := code.Valid(); err != nil {
+	if err = ControlServerSideSortingCode(codeInt).Valid(); err != nil {
 		return nil, err
 	}
 
