32c546031bcb5f1351f696d3fb11ad23eb77a49b
Christopher Puschmann <cp@lumen.sh>
2026-02-05 22:38:24 +0100

    Remove check for REsultCode in WhoAmIResult handling

--- v3/whoami.go
+++ v3/whoami.go
@@ -4,10 +4,6 @@ package ldap
 //
 // https://tools.ietf.org/html/rfc4532
 
-import (
-	"fmt"
-)
-
 // WhoAmIResult is returned by the WhoAmI() call
 type WhoAmIResult struct {
 	AuthzID string
@@ -21,10 +17,6 @@ func (l *Conn) WhoAmI(controls []Control) (*WhoAmIResult, error) {
 	resp, err := l.Extended(extendedRequest)
 	if err != nil {
 		return nil, err
-	}
-
-	if resp.ResultCode != 0 {
-		return nil, NewError(ErrorUnexpectedResponse, fmt.Errorf("Unexpected Response: %d", resp.ResultCode))
 	}
 
 	return &WhoAmIResult{AuthzID: resp.Value.Data.String()}, nil
